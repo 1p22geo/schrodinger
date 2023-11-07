a736940b37cb24c5aa80dd210a6c2e513cb9a740
Geodecki 2P gr.1 <113300767+1p22geo@users.noreply.github.com>
2023-11-07 09:55:59 +0100

    changed script to test hydrogen simulation on CI

--- script2.py
+++ script2.py
@@ -3,14 +3,18 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 import imageio.v2 as imageio
+import scipy
+
+# Bohr radius
+a0 = 1
 
 # Constants and parameters
-Lx = 10.0  # Size of the domain in x-direction
-Ly = 10.0  # Size of the domain in y-direction
-Nx = 1000  # Number of spatial points in x-direction
-Ny = 1000  # Number of spatial points in y-direction
-Nt = 1200  # Number of time points
-T_max = 120  # Maximum time
+Lx = 20.0  # Size of the domain in x-direction
+Ly = 20.0  # Size of the domain in y-direction
+Nx = 2000  # Number of spatial points in x-direction
+Ny = 2000  # Number of spatial points in y-direction
+Nt = 1  # Number of time points
+T_max = 1  # Maximum time
 dt = T_max / Nt  # Time step
 x = np.linspace(0, Lx, Nx)
 y = np.linspace(0, Ly, Ny)
@@ -18,25 +22,69 @@ X, Y = np.meshgrid(x, y)
 dx = x[1] - x[0]
 dy = y[1] - y[0]
 
-# Define the potential function (you can modify this)
-V = np.zeros((Nx, Ny))
-V[:,:] = 1000000
-V[int(0.5* Nx // 8): int(7.5 * Nx // 8),int(0.5* Ny // 8): int(7.5 * Ny // 8)] = 0
-V[:, int(3.5* Ny // 8): int(4 * Ny // 8)] = 1000000
+# Define the potential function (central Coulomb potential)
+x_center = Lx / 2
+y_center = Ly / 2
+r = np.sqrt((X - x_center)**2 + (Y - y_center)**2)
+V = -1 / r  # Central Coulomb potential
 
-# Initial wave function (Gaussial wave packet)
-sigma = 0.5
-kx0 = 2.0
-ky0 = 2.0
-x0 = 1.0
-y0 = 5.0
-psi = np.exp(-((X - x0)**2 + (Y - y0)**2) / (2 * sigma**2)) * np.exp(1j * (kx0 * X + ky0 * Y))
+# Initial wave function (electron in a p-orbital)
+n = 2  # Principal quantum number
+l = 1  # Angular momentum quantum number for p-orbital
+m = 0  # Magnetic quantum number
+r_norm = r / (n * a0)  # Normalize r for the excited state
+theta = np.arctan2(Y - y_center, X - x_center)
+# phi = np.zeros_like(theta)  # Azimuthal angle (0 for simplicity)
+phi = theta
+Ylm = scipy.special.sph_harm(m, l, theta, phi)  # Spherical harmonic
+psi = (2 / (n * a0)**(3/2)) * r_norm * np.exp(-r_norm) * Ylm
+
+
+
+real_part = np.real(psi)
+imag_part = np.imag(psi)
+
+# Create a figure with subplots for 3D projections
+fig = plt.figure(figsize=(12, 4))
+
+# 3D plot of real part
+ax1 = fig.add_subplot(131, projection='3d')
+ax1.plot_surface(X, Y, real_part, cmap='viridis')
+ax1.set_title('Real Part')
+
+ax1.set_xlim(0, Lx)
+ax1.set_ylim(0, Ly)
+ax1.set_zlim(-0.5, 0.5)
+
+# 3D plot of imaginary part
+ax2 = fig.add_subplot(132, projection='3d')
+ax2.plot_surface(X, Y, imag_part, cmap='viridis')
+ax2.set_title('Imaginary Part')
+
+ax2.set_xlim(0, Lx)
+ax2.set_ylim(0, Ly)
+ax2.set_zlim(-0.5, 0.5)
+
+# 3D plot of potential function
+ax3 = fig.add_subplot(133, projection='3d')
+ax3.plot_surface(X, Y, V)
+ax3.set_title('Potential Function')
+
+ax3.set_xlim(0, Lx)
+ax3.set_ylim(0, Ly)
+ax3.set_zlim(-0.5, 0.5)
+
+# Save the figure as a PNG
+filename = f'output_images5/frame_test.png'
+plt.savefig(filename)
+print(f"Saved to {filename}")
+plt.close()
 
 
 # Create a directory to store the PNG images
 import os
-if not os.path.exists("output_images2"):
-    os.makedirs("output_images2")
+if not os.path.exists("output_images5"):
+    os.makedirs("output_images5")
 
 # Lists to store the frames for the video
 frames = []
@@ -65,7 +113,7 @@ for t in range(Nt):
     
     ax1.set_xlim(0, Lx)
     ax1.set_ylim(0, Ly)
-    ax1.set_zlim(-1,1)
+    ax1.set_zlim(-0.5, 0.5)
 
     # 3D plot of imaginary part
     ax2 = fig.add_subplot(132, projection='3d')
@@ -74,18 +122,19 @@ for t in range(Nt):
     
     ax2.set_xlim(0, Lx)
     ax2.set_ylim(0, Ly)
-    ax2.set_zlim(-1,1)
+    ax2.set_zlim(-0.5, 0.5)
 
     # 3D plot of potential function
     ax3 = fig.add_subplot(133, projection='3d')
-    ax3.plot_surface(X, Y, V, cmap='cool')
+    ax3.plot_surface(X, Y, V)
     ax3.set_title('Potential Function')
     
     ax3.set_xlim(0, Lx)
     ax3.set_ylim(0, Ly)
+    ax3.set_zlim(-0.5, 0.5)
 
     # Save the figure as a PNG
-    filename = f'output_images2/frame_{t:03d}.png'
+    filename = f'output_images5/frame_{t:03d}.png'
     plt.savefig(filename)
     print(f"Saved to {filename}")
     plt.close()
@@ -93,6 +142,6 @@ for t in range(Nt):
     frames.append(imageio.imread(filename))
     
 # Create an animated GIF from the frames
-imageio.mimsave('wave_animation2.gif', frames, duration=0.1)
+imageio.mimsave('wave_animation5.gif', frames, duration=0.1)
 
 print("Done! Saved PNG images and created an animated GIF.")
